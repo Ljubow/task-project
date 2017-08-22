c2ccb6f923750c7b8414ece08d3ea2e5c392f6bc
Ljubow <lubovobrusnik@gmail.com>
2017-08-22 22:11:09 +0300

    feature/view-models

--- js/index.js
+++ js/index.js
@@ -5,193 +5,281 @@
 function ready() {
     console.log('Dom ready');
     const cars = {
-        "id": 10,
-        "name": "cars",
-        "children": [{
-            "id": 20,
-            "name": "BMW",
-            "link": "https://www.bmw.com",
-            "top_speed": 210,
-            "fuel": 7.7,
-            "car": "BMW 3 Series Sedan",
-            "url": "img/BMW-3-Series-Sedan_ModelCard.png",
+            "id": 10,
+            "name": "cars",
             "children": [{
-                    "id": 30,
-                    "name": "Sedans",
+                    "id": 20,
+                    "name": "BMW",
+                    "link": "https://www.bmw.com",
+                    "models": "Sedan, Sports Activity Coupe, Touring, Sports Activity Vehicle, Convertible, Hatch, Coupe, Tourer, Gran Turismo, BMW i, Gran Coupe",
+                    "url": "img/BMW-3-Series-Sedan_ModelCard.png",
                     "children": [{
-                        "id": 31,
-                        "name": "BMW 3 Series Sedan",
-                        "top_speed": 210,
-                        "price": 40.000,
-                        "fuel": 7.7,
-                        "url": "BMW-3-Series-Sedan_ModelCard.png",
-                    }]
+                            "id": 31,
+                            "name": "BMW 3 Series Sedan",
+                            "top_speed": 210,
+                            "price": 40.000,
+                            "fuel": 7.7,
+                            "url": "BMW-3-Series-Sedan_ModelCard.png"
+                        },
+                        {
+                            "id": 32,
+                            "name": "BMW 330e plug-in hybrid",
+                            "top_speed": 210,
+                            "price": 34.235,
+                            "fuel": 7.7,
+                            "url": "bmw-iperformance-330e-flyout-890x501.png"
+                        },
+                        {
+                            "id": 33,
+                            "name": "BMW 5 Series Sedan",
+                            "top_speed": 250,
+                            "price": 62.400,
+                            "fuel": 8.1,
+                            "url": "bmw-modelfinder-g30-posi-stage2-890x501.png"
+                        },
+                        {
+                            "id": 35,
+                            "name": "BMW X4 M40i",
+                            "top_speed": 247,
+                            "price": 65.000,
+                            "fuel": 8.3,
+                            "url": "bmw-mperformance-x4-m40i-flyout-890x501.png"
+                        },
+                        {
+                            "id": 36,
+                            "name": "BMW X6",
+                            "top_speed": 25,
+                            "price": 63.495,
+                            "fuel": 8.6,
+                            "url": "BMW-X6_ModelCard.png"
+                        },
+                        {
+                            "id": 38,
+                            "name": "The BMW 2 Series Convertible",
+                            "top_speed": 250,
+                            "price": 48.345,
+                            "fuel": 8.3,
+                            "url": "BMW-2series-cabrio-modelfinder-stage2-890x501-01.png"
+                        },
+                        {
+                            "id": 39,
+                            "name": "BMW M240i Convertible",
+                            "top_speed": 250,
+                            "price": 45.500,
+                            "fuel": 8.3,
+                            "url": "BMW-2series-cabrio-modelfinder-stage2-890x501-03.png"
+                        },
+                        {
+                            "id": 40,
+                            "name": "BMW 4 Series Convertible",
+                            "top_speed": 250,
+                            "price": 76.200,
+                            "fuel": 8.8,
+                            "url": "BMW-4-Series-Convertible-ModelCard.png.jpg"
+                        }
+                    ]
                 },
                 {
-                    "id": 34,
-                    "name": "Sports Activity Coupes",
+                    "id": 21,
+                    "name": "Mercedes-Benz",
+                    "link": "https://www.mbusa.com",
+                    "models": "Sedans & Wagons, Coupes, SUVs, Convertibles & Roadsters, Hybrid & Electric",
+                    "url": "img/2017-C-SEDAN-AV-D.png",
                     "children": [{
-                        "id": 41,
-                        "name": "BMW X4",
-                        "top_speed": 247,
-                        "price": 46.545,
-                        "fuel": 8.3,
-                        "url": "BMW-X4_ModelCard.png"
-                    }]
+                            "id": 42,
+                            "name": "C-CLASS SEDAN",
+                            "top_speed": 241,
+                            "price": 40.250,
+                            "fuel": 8.1,
+                            "url": "2017-C-SEDAN-AV-D.png"
+                        },
+                        {
+                            "id": 43,
+                            "name": "E-Class Sedan",
+                            "top_speed": 241,
+                            "price": 52.950,
+                            "fuel": 7.7,
+                            "url": "2017-E-SEDAN-AV-D.png"
+                        },
+                        {
+                            "id": 44,
+                            "name": "S-Class Sedan",
+                            "top_speed": 430,
+                            "price": 96.600,
+                            "fuel": 7.7,
+                            "url": "2017-S-SEDAN-AV-D.png"
+                        },
+                        {
+                            "id": 46,
+                            "name": "CLA 250 Coupe",
+                            "top_speed": 258,
+                            "price": 32.700,
+                            "fuel": 8.1,
+                            "url": "2017-CLA-COUPE-AV-D.png"
+                        },
+                        {
+                            "id": 47,
+                            "name": "C-Class Coupe",
+                            "top_speed": 258,
+                            "price": 43.200,
+                            "fuel": 8.1,
+                            "url": "2017-C-COUPE-AV-D.png"
+                        },
+                        {
+                            "id": 48,
+                            "name": "E-Class Coupe",
+                            "top_speed": 273,
+                            "price": 58.900,
+                            "fuel": 8.3,
+                            "url": "2018-E-E400-COUPE-D.png"
+                        },
+                        {
+
+                            "id": 81,
+                            "name": "GLA SUV",
+                            "top_speed": 243,
+                            "price": 33.400,
+                            "fuel": 7.7,
+                            "url": "2018-GLA-GLA250-SUV-AV-D.png"
+                        },
+                        {
+                            "id": 50,
+                            "name": "GLC SUV",
+                            "top_speed": 258,
+                            "price": 40.050,
+                            "fuel": 7.7,
+                            "url": "2017-GLC-SUV-AV-D.png"
+                        },
+                        {
+                            "id": 51,
+                            "name": "GLC Coupe",
+                            "top_speed": 265,
+                            "price": 46.600,
+                            "fuel": 8.1,
+                            "url": "2017-GLC-COUPE-AV-D.png"
+                        }
+                    ]
                 },
                 {
-                    "id": 37,
-                    "name": "Convertibles",
+                    "id": 22,
+                    "name": "Volkswagen",
+                    "link": "http://www.vw.com/",
+                    "models": "SEDANS, SUVs, WAGONS, COMPACTS, CONVERTIBLES",
+                    "url": "img/iris.png",
                     "children": [{
-                        "id": 38,
-                        "name": "The BMW 2 Series Convertible",
-                        "top_speed": 250,
-                        "price": 48.345,
-                        "fuel": 8.3,
-                        "url": "BMW-2series-cabrio-modelfinder-stage2-890x501-01.png"
-                    }]
+                            "id": 53,
+                            "name": "Passat S",
+                            "top_speed": 170,
+                            "tuel": 6.7,
+                            "price": 22.440,
+                            "url": "iris.png"
+                        }, {
+                            "id": 54,
+                            "name": "Passat R-Line",
+                            "top_speed": 170,
+                            "fuel": 6.7,
+                            "price": 23.975,
+                            "url": "iris (1).png"
+                        }, {
+                            "id": 55,
+                            "name": "Passat SE",
+                            "top_speed": 195,
+                            "fuel": 6.7,
+                            "price": 25.495,
+                            "url": "iris (2).png"
+                        },
+                        {
+                            "id": 57,
+                            "name": "S (Later availability)",
+                            "top_speed": 235,
+                            "fuel": 6,
+                            "price": 30.500,
+                            "url": "iris (3).png"
+                        }, {
+                            "id": 58,
+                            "name": "Launch Edition",
+                            "top_speed": 250,
+                            "fuel": 6.8,
+                            "price": 33.500,
+                            "url": "iris (4).png"
+                        }, {
+                            "id": 59,
+                            "name": "Launch Edition with 4MOTION",
+                            "top_speed": 250,
+                            "fuel": 7.7,
+                            "price": 35.300,
+                            "url": "iris (5).png"
+                      },
+                        {
+
+                                "id": 61,
+                                "name": "Bettle SEL",
+                                "top_speed": 170,
+                                "fuel": 5.8,
+                                "price": 25.975,
+                                "url": "iris (6).png"
+                            }, {
+                                "id": 62,
+                                "name": "Golf Wolfsburg",
+                                "top_speed": 170,
+                                "fuel": 6,
+                                "price": 21.595,
+                                "url": "iris (7).png"
+                            }, {
+                                "id": 63,
+                                "name": "Golf GTI Sport",
+                                "top_speed": 220,
+                                "fuel": 7.1,
+                                "price": 27.995,
+                                "url": "iris (8).png"
+                            }]
+                        }
+                      ]};
+
+                let content = '';
+                const len = cars.children.length;
+                for (i = 0; i < len; i++) {
+
+                    content += `<div class="card-deck">` +
+                        `<div class='card text-white bg-secondary mb-3' style='max-width: 20rem;'>` +
+                        `<img class='scard-img-top' src='${cars.children[i].url}' alt='Card image cap'>` +
+                        `<div class='card-body'>` +
+                        `<div class='card-header'><div>${cars.children[i].name}</div></div>` +
+                        `<p class='card-name'><div class='row'><div class='col-sm-12'>Link :</div><div class='col-sm-12'>${cars.children[i].link}</div></div></p>` +
+                        `<button type='button' id='myBtn' class='btn btn-outline-light' data-toggle='modal' data-target='#${cars.children[i].id}'>View Models</button></br></br>` +
+                        `<div  id='${cars.children[i].id}' class='modal'></div>` +
+                        `<button type='button' class='btn btn-outline-light' data-toggle='collapse' data-target='#${cars.children[i].name}'>More</button>` +
+                        `</div>` +
+                        `<div id='${cars.children[i].name}' class='collapse'>` +
+                        `<p class='card-link'><div class='row'><div class='col-sm-4'>Models:</div><div class='col-sm-4'>${cars.children[i].models}</div></div></p>` +
+                        `</div>` +
+                        `</div>` +
+                        `</div>`;
                 }
-            ]
-        }, {
-            "id": 21,
-            "name": "Mercedes-Benz",
-            "link": "https://www.mbusa.com",
-            "top_speed": 241,
-            "fuel": 8.1,
-            "car": "C-CLASS SEDAN",
-            "url": "img/2017-C-SEDAN-AV-D.png",
-            "children": [{
-                    "id": 41,
-                    "name": "Sedans & Wagons",
-                    "children": [{
-                        "id": 42,
-                        "name": "C-CLASS SEDAN",
-                        "top_speed": 241,
-                        "price": 40.250,
-                        "fuel": 8.1,
-                        "url": "2017-C-SEDAN-AV-D.png"
-                    }]
-                },
-                {
-                    "id": 45,
-                    "name": "Coupes",
-                    "children": [{
-                        "id": 46,
-                        "name": "CLA 250 Coupe",
-                        "top_speed": 258,
-                        "price": 32.700,
-                        "fuel": 8.1,
-                        "url": "2017-CLA-COUPE-AV-D.png"
-                    }]
-                },
-                {
-                    "id": 49,
-                    "name": "SUVs",
-                    "children": [{
-                        "id": 81,
-                        "name": "GLA SUV",
-                        "top_speed": 243,
-                        "price": 33.400,
-                        "fuel": 7.7,
-                        "url": "2018-GLA-GLA250-SUV-AV-D.png"
-                    }]
+                const elem = document.getElementById('first');
+                console.log(elem);
+                elem.innerHTML = content;
+
+                let modelsC = '';
+                const ln = cars.children[0].children.length;
+                for (i = 0; i < ln; i++) {
+                    modelsC += `<div class='row'><div class='col-sm-6'>Models :</div><div class='col-sm-6'>${cars.children[0].children[i].name}</div></div>`+
+                     `<div class='row'><div class='col-sm-6'>Price :</div><div class='col-sm-6'>${cars.children[0].children[i].price}</div></div>`;
                 }
-            ]
-        }, {
-            "id": 22,
-            "name": "Volkswagen",
-            "link": "http://www.vw.com/",
-            "top_speed": 170,
-            "fuel": 6.7,
-            "car": "Passat S",
-            "url": "img/iris.png",
-            "children": [{
-                    "id": 52,
-                    "name": "Sedans",
-                    "children": [{
-                        "id": 53,
-                        "name": "Passat S",
-                        "top_speed": 170,
-                        "fuel": 6.7,
-                        "price": 22.440,
-                        "url": "iris.png"
-                    }]
-                },
-                {
-                    "id": 56,
-                    "name": "SUVs",
-                    "children": [{
-                        "id": 57,
-                        "name": "S (Later availability)",
-                        "top_speed": 235,
-                        "fuel": 6,
-                        "price": 30.500,
-                        "url": "iris (3).png"
-                    }]
-                },
-                {
-                    "id": 60,
-                    "name": "COMPACTS",
-                    "children": [{
-                        "id": 61,
-                        "name": "Bettle SEL",
-                        "top_speed": 170,
-                        "fuel": 5.8,
-                        "price": 25.975,
-                        "url": "iris (6).png"
-                    }]
+
+
+                const btn = document.getElementById("myBtn");
+                if (btn.addEventListener) {
+                    btn.addEventListener("click", myFunction);
                 }
-            ]
-        }]
-    };
 
-    let content = '';
-    const len = cars.children.length;
-    for (i = 0; i < len; i++) {
+                function myFunction() {
+                    let models = document.querySelector('div.modal-body');
+                    models.innerHTML = modelsC;
 
-        content += `<div class="card-deck">` +
-            `<div class='card text-white bg-secondary mb-3' style='max-width: 350px;'>` +
-            `<img class='scard-img-top' src='${cars.children[i].url}' alt='Card image cap'>` +
-            `<div class='card-body'>` +
-            `<div class='card-header'><div>${cars.children[i].name}</div></div>` +
-            `<p class='card-name'><div class='row'><div class='col-sm-12'>Link :</div><div class='col-sm-12'>${cars.children[i].link}</div></div></p>` +
-            `<button type='button' class='btn btn-outline-light' data-toggle='modal'  id='myBtn' data-target=${cars.children[i].name}>View Models</button></br></br>` +
-            `<button type='button' class='btn btn-outline-light' data-toggle='collapse' data-target='#${cars.children[i].id}'>More</button>` +
-            `</div>` +
-`<div id='${cars.children[i].name}' class='modal'>` +
-            `<div id='${cars.children[i].id}' class='collapse'>` +
-            `<p class='card-link'><div class='row'><div class='col-sm-6'>Link:</div><div class='col-sm-6'>${cars.children[i].link}</div></div></p>` +
-            `<p class='card-top_speed'><div class='row'><div class='col-sm-6'>Top-speed:</div><div class='col-sm-6'>${cars.children[i].top_speed}</div></div></p>` +
-            `<p class='card-fuel'><div class='row'><div class='col-sm-6'>Fuel:</div><div class='col-sm-6'>${cars.children[i].fuel}</div></div></p>` +
-            `</div>` +
-            `</div>` +
-            `</div>`;
-    }
-    const elem = document.getElementById('first');
-    console.log(elem);
-    elem.innerHTML = content;
-
-    let modelsC = '';
-    const ln = cars.children.children;
-    for (i = 0; i < ln; i++) {
-        modelsC += `<div>${cars.children.children[i].id}</div>`;
-    }
+                    $('#modal').modal();
+                }
+            };
 
 
-    const btn = document.getElementById("myBtn");
-    if (btn.addEventListener) {
-        btn.addEventListener("click", myFunction);
-    }
-
-    function myFunction() {
-
-let modalContent=document.querySelector('div.modal-body');
-        modalContent.innerHTML = modelsC;
-        $('#modal').modal();
-    }
-    // const element = document.getElementById('modal')
-    // element.innerHTML=modalContent;
-};
-
-
-document.addEventListener('DOMContentLoaded', ready);
+            document.addEventListener('DOMContentLoaded', ready);
